58a1e6f9ad7f4908f6ac66ca6ccc6ecb812390ee
Lindsay Wardell <three060@gmail.com>
2021-05-19 12:11:47 -0700

    added redis disconnect

--- lib/juralen_elixir/cache.ex
+++ lib/juralen_elixir/cache.ex
@@ -3,28 +3,41 @@ defmodule Juralen.Cache do
     case Redix.start_link(System.get_env("REDIS_URL") || "redis://localhost:6379") do
       {:ok, conn} ->
         conn
+
       _ ->
         raise "Unable to connect to Redis"
     end
   end
 
   def set(key, val) do
-    connect!()
-    |> Redix.command(["SET", key, val])
+    conn = connect!()
+    res = Redix.command(conn, ["SET", key, val])
+    Redix.stop(conn)
+
+    res
   end
 
   def get(key) do
-    connect!()
-    |> Redix.command(["GET", key])
+    conn = connect!()
+    res = Redix.command(conn, ["GET", key])
+    Redix.stop(conn)
+
+    res
   end
 
   def get!(key) do
-    connect!()
-    |> Redix.command!(["GET", key])
+    conn = connect!()
+    res = Redix.command!(conn, ["GET", key])
+    Redix.stop(conn)
+
+    res
   end
 
   def delete(key) do
-    connect!()
-    |> Redix.command(["DEL", "KEY", key])
+    conn = connect!()
+    res = Redix.command(conn, ["DEL", "KEY", key])
+    Redix.stop(conn)
+
+    res
   end
 end
